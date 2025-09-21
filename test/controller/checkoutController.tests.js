398a86ddd904b82b2dd0adaf95925f789a2ec8af
Felipe2578 <felipemoralessouza@yahoo.com.br>
2025-09-21 00:27:02 -0300

    Adicionando test com simon

--- test/controller/checkoutController.tests.js
+++ test/controller/checkoutController.tests.js
@@ -21,5 +21,23 @@ describe('Checkout Controller', () => {
             expect(response.body).to.deep.equal({ error: 'Token inválido' });
 
         });
+
+        it('should return 401 if token is invalid', async () => {
+            const checkoutServiceMock = sinon.stub(checkoutService, 'checkout');
+            checkoutServiceMock.throws(new Error('Erro no checkout'));
+            const response = await request(app)
+                .post('/api/checkout')
+                .send({
+                    items: [{ id: 1, quantity: 1 }],
+                    freight: 10,
+                    paymentMethod: 'credit_card',
+                    cardData: { number: '1234', expiry: '12/24', cvv: '123' }
+                })
+            expect(response.status).to.equal(401);
+            expect(response.body).to.deep.equal({ error: 'Token inválido' });
+
+            sinon.restore();
+
+        });
     });
 })
